bcf8498cbf23dea13f3625fb4ca209d919a9b8b2
Walter Alves <walter.arruda.alves@gmail.com>
2017-03-13 18:02:15 -0300

    Add new schema in schema.yml and rename room_id
    Add the properties in schema.yml and rename room_id to room.

--- db/migrate/20170313203052_create_asset_details.rb
+++ db/migrate/20170313203052_create_asset_details.rb
@@ -3,7 +3,7 @@ class CreateAssetDetails < ActiveRecord::Migration[5.0]
     create_table :asset_details do |t|
       t.text  :description
       t.text  :location
-      t.text  :room_id
+      t.text  :room
       t.text  :rack_name
       t.text  :lowest_rack_unit
     end
