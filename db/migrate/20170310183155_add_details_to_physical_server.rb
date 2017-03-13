5c8b094ee854cecf9e4ac4bca01de70d25a2d921
Walter Alves <walter.arruda.alves@gmail.com>
2017-03-13 13:55:08 -0300

    Remove some properties to database normalization
    Adding only support_contact and description in this migration.

--- db/migrate/20170310183155_add_details_to_physical_server.rb
+++ db/migrate/20170310183155_add_details_to_physical_server.rb
@@ -2,9 +2,5 @@ class AddDetailsToPhysicalServer < ActiveRecord::Migration[5.0]
   def change
     add_column  :physical_servers, :support_contact, :string
     add_column  :physical_servers, :description, :string
-    add_column  :physical_servers, :location,  :string
-    add_column  :physical_servers, :room_id, :string
-    add_column  :physical_servers, :rack_name, :string
-    add_column  :physical_servers, :lowest_rack_unit, :string
   end
 end
