74589fa04ac171135641c21d6b97fe1efd17807f
Walter Alves <walter.arruda.alves@gmail.com>
2017-03-21 14:49:32 -0300

    Change the relationship between Physical server and Asset Details
    Now Asset details can be used for any resource.

--- db/migrate/20170313203052_create_asset_details.rb
+++ db/migrate/20170313203052_create_asset_details.rb
@@ -6,7 +6,10 @@ class CreateAssetDetails < ActiveRecord::Migration[5.0]
       t.text  :room
       t.text  :rack_name
       t.text  :lowest_rack_unit
+      t.bigint  :resource_id
+      t.string  :resource_type
       t.timestamps
+      t.index %w(resource_id resource_type),  :name => "index_asset_details_on_resource_id_and_resource_type", :using => :btree
     end
   end
 end
--- db/migrate/20170313203319_add_assets_details_ref_to_physical_server.rb
+++
@@ -1,5 +0,0 @@
-class AddAssetsDetailsRefToPhysicalServer < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :physical_servers, :asset_details, :type => :bigint, :index => true
-  end
-end
